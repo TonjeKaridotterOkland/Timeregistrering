b89bf504143caf6be2528600a1794ac79a5d8394
TonjeKaridotterOkland <55350586+TonjeKaridotterOkland@users.noreply.github.com>
2023-02-21 23:04:45 +0100

    shows the saved timesheet + awful css

--- start.js
+++ start.js
@@ -2,33 +2,49 @@
 var showText = document.getElementById("show-text");
 var hoursRegistered = document.getElementById("number-of-hours");
 var commentToHoursRegistered = document.getElementById("comment");
-//var sendHoursButton = document.getElementById("form-button");
 var numberOfHours = document.getElementById("number-of-hours");
 
-function saveHours(){
-    //Check if validation set in input id="number-of-hours" has passed
-    if(numberOfHours.checkValidity()){
-    //Prevent form to send and remove the information before it has been saved
-        document.getElementById("form-button").addEventListener("click", function(event){
-            event.preventDefault()
-         });
-
+function getTimeSheet(){
     //Get the previously saved values
-        let timeSheet = JSON.parse(localStorage.getItem("timeSheetForPerson")) || [];
-    // Save the current 
+   return JSON.parse(localStorage.getItem("timeSheetForPerson")) || [];
+};
+
+function setTimeSheet(a){
+   //Set values to localstorage
+   window.localStorage.setItem(`timeSheetForPerson`, JSON.stringify(a));
+};
+
+function saveHours(event){
+//Prevent form to send before the information has been saved
+    event.preventDefault()
+
+//Check if validation set in input id="number-of-hours" has passed
+    if(numberOfHours.checkValidity()){
+
+    // Save the current values
         let todaysTimeSheet= {
             hours: hoursRegistered.valueAsNumber,
             comment: commentToHoursRegistered.value
          };
-        timeSheet.push(todaysTimeSheet);
-
-        window.localStorage.setItem(`timeSheetForPerson`, JSON.stringify(timeSheet));
-
-
+        
+    // Push the new values to the timeSheet-array and set in localstorage
+         let timeSheet = getTimeSheet();
+         timeSheet.push(todaysTimeSheet);
+         setTimeSheet(timeSheet);
          showText.innerHTML = "Timene er lagret";
     };
 };
 
+function showTimeSheet(){
+    let timeSheet = getTimeSheet();
+    for (let registration of timeSheet){
 
+document.getElementById("show-the-hours").innerHTML += `<div class="row"><div class="col-2">${registration.hours} </div><div class="col-2"> ${registration.comment}</div> <div class="col-2"><button>x</button></div></div>`;     
+// document.getElementById("show-the-comments").innerHTML += `<div class="">${registration.comment} </div>`;     
+// document.getElementById("show-delete-button").innerHTML += `<button>x</button>`;     
+        console.log("timer " +registration.hours);
+        console.log("kommentar " +registration.comment);
 
-
+    }
+}
+showTimeSheet();
